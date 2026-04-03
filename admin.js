03f839790246ddf2fc42021653e9db26cecf439f
wed20220202-collab <Wed20220202@gmail.com>
2026-04-03 13:36:53 +0900

    admin.js

--- admin.js
+++ admin.js
@@ -3,7 +3,9 @@ const ADMIN_EMAILS = new Set([
   "vga29-pc250006@sankogakuen.jp",
   "vga29-pc250029@sankogakuen.jp",
   "12210295@sankogakuen.jp",
-  "12010311@sankogakuen.jp"
+  "12010311@sankogakuen.jp",
+  "kousei10160926@gmail.com",
+  "sohuta0810@gmail.com"
 ]);
 
 export function isAdmin(email){
